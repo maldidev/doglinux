e64328006392fb30f203f061e02dd378d54e3614
maldidev <m96023646@gmail.com>
2025-07-06 10:53:58 +0300

    Create open.sc.h

---
+++ lib/open.sc.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int PYSYS = 91;
+int SCAA_DEF()          
+{
+  return -1;
+}
+
+int AA55()
+{
+  int i;
+  int ASC = {
+    0x7A, 0xB7, 0xB8,
+    0,0,0,0,0,0,0,0,0,
+    0xFA, 0xF7, 0x2E
+  };
+  return -PYSYS;
+}
